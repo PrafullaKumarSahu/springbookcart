2b233213c6e8b5d837f0ce050eb24c5c003d09d0
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2022-01-01 19:34:45 +0530

    use lombook

--- src/main/java/com/serverwarrior/springbootcart/dto/Category.java
+++ src/main/java/com/serverwarrior/springbootcart/dto/Category.java
@@ -1,9 +1,15 @@
 package com.serverwarrior.springbootcart.dto;
 
 import com.mongodb.client.model.Collation;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Document(collection = "category")
 public class Category {
 
@@ -12,36 +18,4 @@ public class Category {
     private String name;
     private String description;
     private Integer parent;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getParent() {
-        return parent;
-    }
-
-    public void setParent(Integer parent) {
-        this.parent = parent;
-    }
 }
--- src/main/java/com/serverwarrior/springbootcart/dto/Product.java
+++ src/main/java/com/serverwarrior/springbootcart/dto/Product.java
@@ -1,10 +1,16 @@
 package com.serverwarrior.springbootcart.dto;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Document(collection = "product")
 public class Product {
 
@@ -18,76 +24,4 @@ public class Product {
     private String discountDescription;
     private Category category;
     private List<String> imageURLs;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
-
-    public String getDiscountDescription() {
-        return discountDescription;
-    }
-
-    public void setDiscountDescription(String discountDescription) {
-        this.discountDescription = discountDescription;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public List<String> getImageURLs() {
-        return imageURLs;
-    }
-
-    public void setImageURLs(List<String> imageURLs) {
-        this.imageURLs = imageURLs;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
 }
